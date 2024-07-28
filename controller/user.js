580eed6c153ca4424917bbce4ee7cb48f7b96e07
@dewani12 <gauravdewani2@gmail.com>
2024-07-28 11:37:50 +0530

    mongoose validation

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,10 @@
 import User from '../model/user.js'
 import bcrypt from 'bcryptjs'
 
+// const handleErrors = (err) => {
+//     console.log(err.message, err.code)
+// }
+
 export const signup = async (req, res) => {
     try {
         const { fullname, email, password } = req.body
@@ -21,6 +25,7 @@ export const signup = async (req, res) => {
         await createdUser.save()
         return res.status(201).json({ mssg: "User created!" })
     } catch (error) {
+        // handleErrors(error)
         console.log("Error: " + error)
         return res.status(500).json("Internal server error")
     }
@@ -38,6 +43,7 @@ export const login = async (req, res) => {
             return res.status(200).json({ mssg: "User logged in!" })
         }
     } catch (error) {
+        // handleErrors(error)
         console.log("Error: " + error)
         return res.status(500).json({ mssg: "Internal server error" })
     }
--- model/user.js
+++ model/user.js
@@ -1,18 +1,24 @@
 import mongoose from 'mongoose'
+import pkg from 'validator'
+
+const {isEmail} = pkg
 
 const userSchema = mongoose.Schema({
     fullname: {
         type: String,
-        required: true
+        required: [true, 'Please enter a fullname']
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail,  'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
 })
 
